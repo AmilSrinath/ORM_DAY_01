1a37a3772410972930bb8ecf3e9c5a3042274a2c
Amil Srinath <amilsrinath5@gmail.com>
2023-07-21 11:17:22 +0530

    Ended Create FactoryConfiguration.java

--- src/main/java/org/example/utill/FactoryConfiguration.java
+++ src/main/java/org/example/utill/FactoryConfiguration.java
@@ -1,8 +1,21 @@
 package org.example.utill;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class FactoryConfiguration {
     private static FactoryConfiguration factoryConfiguration;
-    private FactoryConfiguration(){}
+    private SessionFactory sessionFactory;
+    private FactoryConfiguration(){
+        Configuration configuration = new Configuration();
+    }
 
+    public static FactoryConfiguration getInstance(){
+        return (factoryConfiguration==null) ? factoryConfiguration=new FactoryConfiguration():factoryConfiguration;
+    }
 
+    public Session getSession(){
+        return sessionFactory.openSession();
+    }
 }
